641111f78ff80578d5088d9e68341a9b9f03fed7
Matt Kuras <mattkuras@gmail.com>
2020-02-25 23:35:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
  def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)[:get_emoticon][emoticon]
   if load_library(file_path)[:get_emoticon][emoticon] === nil 
-    return "sorry"
+    return "Sorry, that emoticon was not found"
 
     
     end
