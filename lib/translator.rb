0bcc1082b877949dc9560b218579c5e449c1e585
Matt Kuras <mattkuras@gmail.com>
2020-02-25 23:20:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
  def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)[:get_meaning].each do |key, value|
   if value === "grinning" 
-     key 
+     return key 
   end
     end
     end
