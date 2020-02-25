de9190b3aa71281fcba89a9bc086a7d7aa9c1296
Matt Kuras <mattkuras@gmail.com>
2020-02-25 22:54:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,26 +20,26 @@ end
 #     "not here"
 #   end
   
-#   #load_library(file_path)[:get_meaning].each do |key, value|
-# #   if value === "happy"
-# #     key 
-# #     end
-# #   if value === "grinning" 
-# #   key
-# # end
-#   end
+  #load_library(file_path)[:get_meaning].each do |key, value|
+#   if value === "happy"
+#     key 
+#     end
+#   if value === "grinning" 
+#   key
 # end
-
-def get_japanese_emoticon(file_path, emoticon)
-  binding.pry 
-  testing = load_library(file_path)[:get_meaning][emoticon]
-  if testing != nil 
-    testing 
-  else 
-    "not here"
   end
-  binding.pry 
 end
+
+# def get_japanese_emoticon(file_path, emoticon)
+#   binding.pry 
+#   testing = load_library(file_path)[:get_meaning][emoticon]
+#   if testing != nil 
+#     testing 
+#   else 
+#     "not here"
+#   end
+#   binding.pry 
+# end
 
 def get_english_meaning
   # code goes here
